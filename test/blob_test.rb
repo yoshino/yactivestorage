6fad235bf138cfe9a87cf70bd61b53c8364178be
Taiki Yoshino <44nocycle@gmail.com>
2018-07-27 22:03:12 +0900

    Quote this!

--- test/blob_test.rb
+++ test/blob_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Yactivestorage::BlobTest < ActiveSuppoer::TestCase
   test "truth" do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-require 'bundler/setup'
-require 'active_support'
-require 'active_support/testing/autorun'
-require 'pry'
+require "bundler/setup"
+require "active_support"
+require "active_support/testing/autorun"
+require "pry"
