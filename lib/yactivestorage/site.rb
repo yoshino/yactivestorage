ceab6f8f7a7dfef01a26685ee7c70f5c78de7621
Taiki Yoshino <yoshino@example.com>
2018-07-28 23:37:59 +0900

    Underscore its an interface

--- lib/yactivestorage/site.rb
+++ lib/yactivestorage/site.rb
@@ -1,26 +1,38 @@
+# Abstract class serving as an interface for concrete sites.
 class Yactivestorage::Site
+  def initialize
+  end
+
   def upload(key, data)
+    raise NoImplementadError
   end
 
   def download(key)
+    raise NoImplementadError
   end
 
   def delete(key)
+    raise NoImplementadError
   end
 
   def exists?(key)
+    raise NoImplementadError
   end
 
   def url(key)
+    raise NoImplementadError
   end
 
   def checksum(key)
+    raise NoImplementadError
   end
 
   def copy(form_key:, to_key:)
+    raise NoImplementadError
   end
 
   def move(from_key:, to_key:)
+    raise NoImplementadError
   end
 end
 
