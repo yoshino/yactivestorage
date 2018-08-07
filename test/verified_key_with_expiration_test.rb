e584a5bdab9ec5447bf045078d23deb1e970b3aa
Taiki Yoshino <44nocycle@gmail.com>
2018-08-07 22:39:50 +0900

    Fix and test expiration

--- test/verified_key_with_expiration_test.rb
+++ test/verified_key_with_expiration_test.rb
@@ -8,4 +8,12 @@ class Yactivestorage::VerifiedKeyWithExpirationTest < ActiveSupport::TestCase
     encoded_key = Yactivestorage::VerifiedKeyWithExpiration.encode(FIXTURE_KEY)
     assert_equal FIXTURE_KEY, Yactivestorage::VerifiedKeyWithExpiration.decode(encoded_key)
   end
+
+  test "with expiration" do
+    encoded_key = Yactivestorage::VerifiedKeyWithExpiration.encode(FIXTURE_KEY, expires_in: 1.minute)
+    assert_equal FIXTURE_KEY, Yactivestorage::VerifiedKeyWithExpiration.decode(encoded_key)
+
+    travel 2.minutes
+    assert_nil Yactivestorage::VerifiedKeyWithExpiration.decode(encoded_key)
+  end
 end
