75953fffae5d1efa175ac9bfbf44e05428ee96f2
Taiki Yoshino <44nocycle@gmail.com>
2018-08-11 12:38:49 +0900

    Extract explaining methods

--- lib/yactivestorage/verified_key_with_expiraion.rb
+++ lib/yactivestorage/verified_key_with_expiraion.rb
@@ -1,15 +1,24 @@
 class Yactivestorage::VerifiedKeyWithExpiration
   class_attribute :verifier, default: defined?(Rails) ? Rails.application.message_verifier('Yactivestorage') : nil
 
-  def self.encode(key, expires_in: nil)
-    verifier.generate([ key, expires_in ? Time.now.utc.advance(seconds: expires_in) : nil ])
-  end
+  class << self
 
-  def self.decode(encoded_key)
-    key, expires_at = verifier.verified(encoded_key)
-
-    if key
-      key if expires_at.nil? || Time.now.utc < expires_at
+    def encode(key, expires_in: nil)
+      verifier.generate([ key, expires_at(expires_in) ])
     end
+
+    def decode(encoded_key)
+      key, expires_at = verifier.verified(encoded_key)
+      key if key && fresh?(expires_at)
+    end
+
+    private
+      def expires_at(expires_in)
+        expires_in ? Time.now.utc.advance(seconds: expires_in) : nil
+      end
+
+      def fresh?(expires_at)
+        expires_at.nil? || Time.now.utc < expires_at
+      end
   end
 end
