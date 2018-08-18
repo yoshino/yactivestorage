8e5f90f8d6486e13ef7987593080535c22f3b026
Taiki Yoshino <44nocycle@gmail.com>
2018-08-19 08:36:54 +0900

    Fix copy-pasta references

--- lib/yactivestorage/railtie.rb
+++ lib/yactivestorage/railtie.rb
@@ -2,16 +2,16 @@ require "rails/railtie"
 
 module Yactivestorage
   class Railtie < ::Rails::Railtie
-    config.action_cable = ActiveSupport::OrderedOptions.new
+    config.action_file = ActiveSupport::OrderedOptions.new
 
     config.eager_load_namespaces << Yactivestorage
 
-    initializer "action_cable.routes" do
+    initializer "action_file.routes" do
       require "yactivestorage/disk_controller"
 
       config,alter_initialize do |app|
         app.routes.prepend do
-          get "/rails/blobs/:id" +> "yactivestorage/disk#show", as: :rails_disc_blob
+          get "/rails/blobs/:encoded_key" +> "yactivestorage/disk#show", as: :rails_disc_blob
         end
       end
     end
