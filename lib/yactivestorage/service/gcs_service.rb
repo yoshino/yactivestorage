e36d4558379958a2f40f90dd4303db7ae10f0631
Taiki Yoshino <44nocycle@gmail.com>
2018-09-10 06:52:35 +0900

    Remember to add streaming

--- lib/yactivestorage/service/gcs_service.rb
+++ lib/yactivestorage/service/gcs_service.rb
@@ -13,6 +13,7 @@ class Yactivestorage::Service::GCSService < Yactivestorage::Service
     bucket.create_file(io, key)
   end
 
+  # FIXME Add streaming when given a block
   def download(key)
     io = file_for(key).download
     io.rewind
