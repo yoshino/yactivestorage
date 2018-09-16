2b985f944d4cc7cebca93d33429bc715f57210d4
Taiki Yoshino <44nocycle@gmail.com>
2018-09-16 20:27:58 +0900

    Change type of created_at columns from time to datetime

--- lib/yactivestorage/migration.rb
+++ lib/yactivestorage/migration.rb
@@ -1,23 +1,23 @@
 class YactivestorageCreateTables < ActiveRecord::Migration[5.2]
   def change
     create_table :yactivestorage_blobs do |t|
-      t.string :key
-      t.string :filename
-      t.string :content_type
-      t.text :metadata
-      t.integer :byte_size
-      t.string :checksum
-      t.time :created_at
+      t.string   :key
+      t.string   :filename
+      t.string   :content_type
+      t.text     :metadata
+      t.integer  :byte_size
+      t.string   :checksum
+      t.datetime :created_at
 
       t.index [ :key ], unique: true
     end
 
     create_table :yactivestorage_attachments do |t|
-      t.string :name
-      t.string :record_gid
-      t.string :blob_id
+      t.string   :name
+      t.string   :record_gid
+      t.string   :blob_id
 
-      t.time :created_at
+      t.datetime :created_at
 
       t.index :record_gid
       t.index :blob_id
