5572b2e3c286bb32024b62f01d4e3d1c6afbe396
Taiki Yoshino <44nocycle@gmail.com>
2018-09-28 05:56:38 +0900

    Test URL generation for S3 and DISK

--- test/service/s3_service_test.rb
+++ test/service/s3_service_test.rb
@@ -5,6 +5,11 @@ if SERVICE_CONFIGURATIONS[:s3]
     SERVICE = ActiveStorage::Service.configure(:s3, SERVICE_CONFIGURATIONS)
 
     include ActiveStorage::Service::SharedServiceTests
+
+    test "signed URL generation" do
+      assert_match /rails-activestorage\.s3\.amazonaws\.com.*response-content-disposition=inline.*avatar\.png/,
+        @service.url(FIXTURE_KEY, expires_in: 5.minutes, disposition: :inline, filename: "avatar.png") # 5.minutesのところでエラー（railsのメソッドが使えない＋s3のバージョン変更）
+    end
   end
 else
   puts "Skipping S3 Service tests because no S3 configuration was supplied"
