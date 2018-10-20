515e6c3c296646a29d47a2c6acc34ce415c72186
Taiki Yoshino <44nocycle@gmail.com>
2018-10-20 11:24:43 +0900

    Actually we just want want them mounted straight

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
-ActiveStorage::Engine.routes.draw do
+Rails.application.routes.draw do
   get  "/rails/active_storage/disk/:encoded_key/*filename" => "active_storage/disk#show", as: :rails_disk_blob
-  get  "/rails/active_storage/variants/:encoded_blob_key/:variation_key/*filename" => "active_storage/variants#show", as: :rails_blob_variant
   post "/rails/active_storage/direct_uploads" => "active_storage/direct_uploads#create", as: :rails_direct_uploads
-   resolve 'ActiveStorage::Variant' do |variant|
-    encoded_blob_key    = ActiveStorage::VerifiedKeyWithExpiration.encode(variant.blob.key)
-    variation_key = ActiveStorage::Variant.encode_key(variant.variation)
-    filename            = variant.blob.filename
-     route_for(:rails_blob_variant, encoded_blob_key, variation_key, filename)
+
+  get  "/rails/active_storage/variants/:encoded_blob_key/:variation_key/*filename" => "active_storage/variants#show", as: :rails_blob_variation
+
+  direct :rails_variant do |variant|
+    encoded_blob_key = ActiveStorage::VerifiedKeyWithExpiration.encode(variant.blob.key)
+    variation_key    = variant.variation.key
+    filename         = variant.blob.filename
+
+    route_for(:rails_blob_variation, encoded_blob_key, variation_key, filename)
   end
+
+  resolve 'ActiveStorage::Variant' { |variant| route_for(:rails_variant, variant) }
 end
