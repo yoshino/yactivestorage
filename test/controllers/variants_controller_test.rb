66e821b64045b1d44a0a15ab248b8776a0052351
Taiki Yoshino <44nocycle@gmail.com>
2018-10-20 12:43:52 +0900

    Even more needless requires

--- test/controllers/variants_controller_test.rb
+++ test/controllers/variants_controller_test.rb
@@ -2,7 +2,6 @@ require "test_helper"
 require "database/setup"
 
 require "active_storage/variants_controller"
-require "active_storage/verified_key_with_expiration"
 
 class ActiveStorage::VariantCollectorTest < ActionController::TestCase
   setup do
